e11317423d27c306913a0ddf91c1ff68c08a0f8c
Peter Whitby <peter.v.whitby@gmail.com>
2022-06-27 16:00:40 +0100

    created and suffle deck

---
+++ src/utils/createDeck.js
@@ -0,0 +1,41 @@
+const suits = ["Spades", "Hearts", "Clubs", "Diamonds"];
+const values = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+const deck = new Array();
+
+const createDeck = () => {
+    for(let i = 0 ; i < suits.length; i++){
+
+        for(let j = 0; j < values.length;j++){
+            
+            let code =  suits[i].substring(0,1) + values[j]
+            let card = { suit: suits[i], value: values[j] , code: code}
+            deck.push(card)
+        }
+    }
+}
+createDeck()
+console.log(deck);
+
+const shuffle = (array) => {
+    let m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+    }
+
+    return array;
+}
+
+const shuffledDeck = shuffle(deck)
+
+console.log(shuffledDeck);
+
+//export default createDeck;
