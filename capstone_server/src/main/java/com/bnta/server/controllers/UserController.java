72cfa5ffd7f337d3772c3870c92cf3b8e1a728f4
Ed Hulbert <edward.hulbert@hotmail.co.uk>
2022-06-29 12:23:11 +0100

    initialised backend

---
+++ capstone_server/src/main/java/com/bnta/server/controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.bnta.server.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+}
---
+++ capstone_server/src/main/java/com/bnta/server/controllers/blackjack/BlackjackStatsController.java
@@ -0,0 +1,9 @@
+package com.bnta.server.controllers.blackjack;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("blackjack_stats")
+public class BlackjackStatsController {
+}
