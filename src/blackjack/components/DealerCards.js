39552f9670a51509b5391b0411f05d51e7a5425c
Patrick Merchant <patrickmerchant9@gmail.com>
2022-07-04 10:10:38 +0100

    cards flipping on deal

--- src/blackjack/components/DealerCards.js
+++ src/blackjack/components/DealerCards.js
@@ -1,15 +1,29 @@
-import "./DealerCards.css";
+import "./Cards.css";
 import { useEffect } from "react";
 
 const DealerCards = ({dealerCards, displayImages, isDealerTurn}) => {
 
-    // useEffect to change JSX custom data attribute for styling flipped card:
-        useEffect(() => {
-            if(isDealerTurn){
-                const element = document.querySelectorAll("#dealer-cards > .card:nth-of-type(2)");
-                element[0].setAttribute("data-flip", "yes");
+    // useEffects to change JSX custom data attribute for styling flipped card:
+
+    // Flip vard 2 on dealer's turn:
+    useEffect(() => {
+        if(isDealerTurn){
+            const element = document.querySelectorAll("#dealer-cards > .card:nth-of-type(2)");
+            element[0].setAttribute("data-flip", "yes");
+        }
+    })
+
+    // Flip card 1 on deal:
+    useEffect(() => {
+        const element = document.querySelectorAll("#dealer-cards > .card");
+        for(let i=0; i<element.length; i++){
+            if(i === 1){
+                continue;
+            } else {
+                element[i].setAttribute("data-flip", "yes")
             }
-        })
+        }
+    })
 
     return(
         <>
--- src/blackjack/components/PlayerCards.js
+++ src/blackjack/components/PlayerCards.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 const PlayerCards = ({playerCards, displayImages}) => {
+
+    // Flip player cards on deal:
+    useEffect(() => {
+        const element = document.querySelectorAll("#player-cards > .card");
+        for(let i=0; i<element.length; i++){
+            element[i].setAttribute("data-flip", "yes");
+        }
+    }, [playerCards])
+
     return (
         <>
         <div id="player-cards">
