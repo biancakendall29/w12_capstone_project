78b138ea8cf6bbeef0d6828afd35316588df1705
Ed Hulbert <edward.hulbert@hotmail.co.uk>
2022-07-04 17:44:34 +0100

    merged main into navbar

--- capstone_server/src/main/java/com/bnta/server/repositories/BlackjackSaveRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bnta.server.repositories;
-
-import com.bnta.server.models.blackjack.BlackjackSave;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlackjackSaveRepository  extends JpaRepository<BlackjackSave, Long> {
-}
